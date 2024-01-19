444c55fd9860c00f292aab1e2b453cca5cc0b3dd
Gabriel Camilo <gabrielcamilom15@gmail.com>
2024-01-19 11:18:33 -0300

    .

---
+++ FirstProjectQuantity/FirstProjectQuantity/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstProjectQuantity
+{
+    public partial class MainPage : ContentPage
+    {
+        private int quantidade = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            number_quantidade.Text = quantidade.ToString();
+            if (quantidade == 0)
+            {
+                button_drop.IsEnabled = false;
+            }
+        }
+
+        private void add_quantidade(object sender, EventArgs e)
+        {
+            quantidade += 1;
+            number_quantidade.Text = quantidade.ToString();
+            if (quantidade > 0)
+            {
+                button_drop.IsEnabled = true;
+            }
+        }
+
+        private void drop_quantidade(object sender, EventArgs e)
+        {
+            if (quantidade == 0)
+            {
+                button_drop.IsEnabled = false;
+            }
+            else {
+                button_drop.IsEnabled = true;
+                quantidade -= 1;
+                number_quantidade.Text = quantidade.ToString();
+                button_drop.IsEnabled = quantidade == 0 ? false : button_drop.IsEnabled; 
+            }
+        }
+    }
+}
